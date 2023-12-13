8eeecae4643c3bdcb4cf6b25146378f02c2291ee
maryrosedevega <143856348+maryrosedevega@users.noreply.github.com>
2023-12-13 22:10:04 +0800

    Delete afrolidxml directory

--- afrolidxml/__init__.py
+++
@@ -1,8 +0,0 @@
-import afrolidxml.meta as meta
-
-__author__ = meta.author
-__copyright__ = meta.copyright_description
-__license__ = meta.license_description
-__version__ = meta.version
-__email__ = meta.email
-__status__ = meta.status
